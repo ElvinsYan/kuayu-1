aff9d3442c942474a8de62bffd4b8c5fa6cc4d8c
ElvinsYan <yanxiangyang94@gmail.com>
2021-11-22 22:54:16 +0800

    init

---
+++ Elvins.com/public/elvins.js
@@ -0,0 +1,37 @@
+function jsonp(url){
+    return new Promise((resolve, reject) => {
+        const random = Math.random()
+        window[random] = (data)=>{
+            resolve(data)
+        }
+        const script = document.createElement('script')
+        script.src = `${url}?callback=${random}`
+        script.onload=()=>{
+            script.remove()
+        }
+        script.onerror=()=>{
+            reject()
+        }
+        document.body.appendChild(script)
+    })
+}
+
+jsonp('http://qq.com:8888/friends.js')
+  .then((data)=>{
+    console.log(data)
+  })
+
+
+
+// 这是CORS
+
+
+//const request = new XMLHttpRequest();
+//request.open("GET", "http://qq.com:8888/friends.json");
+//request.onreadystatechange = () => {
+//  if (request.readyState === 4 && request.status === 200) {
+//    console.log(request.response);
+//  }
+//};
+//
+//request.send();
